360f212751bd42d1a1446b10ab8325e675c8b748
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-09-16 18:45:53 -0700

    Tarea3
    -Como primer paso crea solo la clase con los atributos públicos e inicializalos en Program.Main() directamente.
    -Crea dos objetos tipo Pelicula con dos peliculas ganadoras de un Oscar.
    -Imprime en la consola el titulo y año de las peliculas. Sube la primera versión a GitHub.

---
+++ Tarea3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tarea3
+{
+    class Pelicula
+    {
+        public string Titulo;
+        public Int16 Ano;
+        public string Pais;
+        public string Director;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelicula p1 = new Pelicula();
+            p1.Titulo = "El laberinto del fauno";
+            p1.Ano = 2006;
+            p1.Pais = "Mexico";
+            p1.Director = "Guillermo del Toro";
+
+            //Console.WriteLine("Titulo: {0}\nAno: {1}\nPais: {2}\nDirector: {3}\n", p1.Titulo, p1.Ano, p1.Pais, p1.Director);
+            Console.WriteLine("Titulo: {0}\nAno: {1}\n", p1.Titulo, p1.Ano);
+
+            
+            Pelicula p2 = new Pelicula();
+            p2.Titulo = "La forma del agua";
+            p2.Ano = 2017;
+            p2.Pais = "Estados Unidos";
+            p2.Director = "Guillermo del Toro";
+
+            Console.WriteLine("Titulo: {0}\nAno: {1}\n", p2.Titulo, p2.Ano);
+
+        }
+    }
+}
