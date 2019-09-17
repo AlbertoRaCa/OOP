aa9a01843944165110140ba470c1dd57e992de58
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-09-16 17:03:59 -0700

    Tarea 3.1
    Se crea solo la clase con los atributos públicos y se inicializa en Program.Main() directamente.

---
+++ Tarea3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tarea3
+{
+    class Pelicula
+    {
+        public string Titulo;
+        public Int16 Ano;
+        public string Pais;
+        public string Director;
+
+        public void imprime()
+        {
+            Console.WriteLine("Pelicula: {0}\nAno: {1}\nPais: {2}\nDirector: {3}\n", Titulo, Ano, Pais, Director);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelicula p1 = new Pelicula();
+            p1.Titulo = "Gantz";
+            p1.Ano = 2010;
+            p1.Pais = "Japon";
+            p1.Director = "Taro Kawazu";
+
+            p1.imprime();
+        }
+    }
+}
