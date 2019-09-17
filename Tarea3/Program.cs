d26e0ff44a8a7e603d926defe6e5f26c83886f04
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-09-16 19:40:20 -0700

    Tarea3.1
    -Ahora cambia los atributos a private

--- Tarea3/Program.cs
+++ Tarea3/Program.cs
@@ -5,33 +5,45 @@ namespace Tarea3
 {
     class Pelicula
     {
-        public string Titulo;
-        public Int16 Ano;
-        public string Pais;
-        public string Director;
+        private String Titulo;
+        private Int16 Ano;
+        //public String Pais;
+        //public String Director;
+
+        public void SetTitulo(String Titulo)
+        {
+            this.Titulo = Titulo;
+        }
+        public String GetTitulo()
+        {
+            return this.Titulo;
+        }
+        public void SetAno(Int16 Ano)
+        {
+            this.Ano = Ano;
+        }
+        public Int16 GetAno()
+        {
+            return this.Ano;
+        }
 
     }
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
             Pelicula p1 = new Pelicula();
-            p1.Titulo = "El laberinto del fauno";
-            p1.Ano = 2006;
-            p1.Pais = "Mexico";
-            p1.Director = "Guillermo del Toro";
+            p1.SetTitulo("El laberinto del fauno");
+            p1.SetAno(2006);
 
-            //Console.WriteLine("Titulo: {0}\nAno: {1}\nPais: {2}\nDirector: {3}\n", p1.Titulo, p1.Ano, p1.Pais, p1.Director);
-            Console.WriteLine("Titulo: {0}\nAno: {1}\n", p1.Titulo, p1.Ano);
+            Console.WriteLine("Titulo: {0}\nAno: {1}\n", p1.GetTitulo(), p1.GetAno());
 
             
             Pelicula p2 = new Pelicula();
-            p2.Titulo = "La forma del agua";
-            p2.Ano = 2017;
-            p2.Pais = "Estados Unidos";
-            p2.Director = "Guillermo del Toro";
+            p2.SetTitulo("La forma del agua");
+            p2.SetAno(2017);
 
-            Console.WriteLine("Titulo: {0}\nAno: {1}\n", p2.Titulo, p2.Ano);
+            Console.WriteLine("Titulo: {0}\nAno: {1}\n", p2.GetTitulo(), p2.GetAno());
 
         }
     }
