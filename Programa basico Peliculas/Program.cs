668e1ce7b5da75dc5f0081a4d46f0ff4b51321b5
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-09-18 18:19:42 -0700

    TAREA3.1
    Programa básico Películas (20 de Septiembre)
    De acuerdo a los ejemplos vistos en clase y la lectura. Implementa en c# la clase Pelicula con los siguientes atributos publicos:
    Pelicula
    atributo	tipo
    titulo	String
    año	Int16
    país	String
    director	String
    
    1. Como primer paso crea solo la clase con los atributos públicos e inicializalos en Program.Main() directamente.
    2. Crea dos objetos tipo Pelicula con dos peliculas ganadoras de un Oscar.
    3. Imprime en la consola el titulo y año de las peliculas. Sube la primera versión a GitHub.

---
+++ Programa basico Peliculas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Programa_basico_Peliculas
+{
+    class Pelicula
+    {
+        //Atributos publicos
+        public String Titulo;
+        public Int16 Ano;
+        public string Pais;
+        public string Director;        
+
+    }
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Objeto tipo pelicula
+            Pelicula p1 = new Pelicula();
+            p1.Titulo = "El laberinto del fauno";
+            p1.Ano = 2006;
+            p1.Pais = "Mexico";
+            p1.Director = "Gillermo Del Toro";
+            Console.WriteLine("Titulo: {0}\nAno :{1}\n", p1.Titulo, p1.Ano);
+            //Console.WriteLine("Titulo: {0}\nAno :{1}\nPais: {2}\nDirector: {3}\n", p1.Titulo, p1.Ano, p1.Pais, p1.Director);
+
+            Pelicula p2 = new Pelicula();
+            p2.Titulo = "La forma del agua";
+            p2.Ano = 2017;
+            p2.Pais = "Estados unidos";
+            p2.Director = "Gillermo Del Toro";
+            Console.WriteLine("Titulo: {0}\nAno :{1}\n", p2.Titulo, p2.Ano);
+            
+        }
+    }
+}
