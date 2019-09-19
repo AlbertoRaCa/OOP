2b13c4ad26cdda5e194bcbe3de3bfc8a5740364d
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-09-18 18:37:24 -0700

    TAREA3.2
    Ahora cambia los atributos a private. Agrega los métodos necesarios para imprimir
    Actualiza la versión a GitHub.

--- Programa basico Peliculas/Program.cs
+++ Programa basico Peliculas/Program.cs
@@ -4,12 +4,29 @@ namespace Programa_basico_Peliculas
 {
     class Pelicula
     {
-        //Atributos publicos
-        public String Titulo;
-        public Int16 Ano;
+        //Atributos
+        private String Titulo;
+        private Int16 Ano;
         public string Pais;
-        public string Director;        
+        public string Director;
 
+        //Metodos get set para utilizar atributos privados en otras clases
+        public void SetTitulo(string Titulo)
+        {
+            this.Titulo = Titulo;
+        } 
+        public string GetTitulo()
+        {
+            return this.Titulo;
+        }
+        public void SetAno(Int16 Ano)
+        {
+            this.Ano = Ano;
+        } 
+        public Int16 GetAno()
+        {
+            return this.Ano;
+        }
     }
     class Program
     {
@@ -17,19 +34,19 @@ namespace Programa_basico_Peliculas
         {
             //Objeto tipo pelicula
             Pelicula p1 = new Pelicula();
-            p1.Titulo = "El laberinto del fauno";
-            p1.Ano = 2006;
-            p1.Pais = "Mexico";
-            p1.Director = "Gillermo Del Toro";
-            Console.WriteLine("Titulo: {0}\nAno :{1}\n", p1.Titulo, p1.Ano);
+            p1.SetTitulo("El laberinto del fauno");
+            p1.SetAno(2006);
+            //p1.Pais = "Mexico";
+            //p1.Director = "Gillermo Del Toro";
+            Console.WriteLine("Titulo: {0}\nAno :{1}\n", p1.GetTitulo(), p1.GetAno());
             //Console.WriteLine("Titulo: {0}\nAno :{1}\nPais: {2}\nDirector: {3}\n", p1.Titulo, p1.Ano, p1.Pais, p1.Director);
 
             Pelicula p2 = new Pelicula();
-            p2.Titulo = "La forma del agua";
-            p2.Ano = 2017;
-            p2.Pais = "Estados unidos";
-            p2.Director = "Gillermo Del Toro";
-            Console.WriteLine("Titulo: {0}\nAno :{1}\n", p2.Titulo, p2.Ano);
+            p2.SetTitulo("La forma del agua");
+            p2.SetAno(2017);
+            //p2.Pais = "Estados unidos";
+            //p2.Director = "Gillermo Del Toro";
+            Console.WriteLine("Titulo: {0}\nAno :{1}\n", p2.GetTitulo(), p2.GetAno());
             
         }
     }
