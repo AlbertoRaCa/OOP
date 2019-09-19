48e9a12d9656a8aff15ad66e09814c9ebaf0bf01
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-09-18 18:48:00 -0700

    TAREA3.3
    1. Agrega dos constructores: Pelicula() y Pelicula(string titulo, Int16 año ) y el método imprime(). Actualiza la versión a GitHub.

--- Programa basico Peliculas/Program.cs
+++ Programa basico Peliculas/Program.cs
@@ -27,6 +27,24 @@ namespace Programa_basico_Peliculas
         {
             return this.Ano;
         }
+
+        //Primer constructor
+        public Pelicula()
+        {
+
+        }
+        //Segundo constructor
+        public Pelicula(String Titulo, Int16 Ano)
+        {
+            this.Titulo = Titulo;
+            this.Ano = Ano;
+        }
+
+        //metodo imprime
+        public void imprime()
+        {
+            Console.WriteLine("Titulo: {0}\nAno: {1}\n", this.Titulo, this.Ano);
+        }
     }
     class Program
     {
@@ -36,17 +54,21 @@ namespace Programa_basico_Peliculas
             Pelicula p1 = new Pelicula();
             p1.SetTitulo("El laberinto del fauno");
             p1.SetAno(2006);
+
+            p1.imprime();
             //p1.Pais = "Mexico";
             //p1.Director = "Gillermo Del Toro";
-            Console.WriteLine("Titulo: {0}\nAno :{1}\n", p1.GetTitulo(), p1.GetAno());
+            //Console.WriteLine("Titulo: {0}\nAno :{1}\n", p1.GetTitulo(), p1.GetAno());
             //Console.WriteLine("Titulo: {0}\nAno :{1}\nPais: {2}\nDirector: {3}\n", p1.Titulo, p1.Ano, p1.Pais, p1.Director);
 
             Pelicula p2 = new Pelicula();
             p2.SetTitulo("La forma del agua");
             p2.SetAno(2017);
+
+            p2.imprime();
             //p2.Pais = "Estados unidos";
             //p2.Director = "Gillermo Del Toro";
-            Console.WriteLine("Titulo: {0}\nAno :{1}\n", p2.GetTitulo(), p2.GetAno());
+            //Console.WriteLine("Titulo: {0}\nAno :{1}\n", p2.GetTitulo(), p2.GetAno());
             
         }
     }
