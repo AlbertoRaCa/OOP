e348c8f4b7411f34cce3ab552ce3f6b75d8ac8c1
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-09-24 19:59:30 -0700

    espia
    Clase martes 24 septiembre 2019

---
+++ espias/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace espias
+{
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+
+        public Persona()
+        {
+            nombre = "Fulano";
+            apellido = "De Tal";
+        }   
+    }
+    class Annonymus
+    {
+        public static void anonimiza(Persona p)
+        {
+            p.nombre = "xxxxxxx";
+            p.apellido = "xxxxxxx";
+        }
+    }
+    public static void cambia(ref Persona p)
+    {// se cambia el a otro nombre
+        p = new Persona();
+        p.nombre = "new";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {       //referencia a p
+            Persona p = new Persona();
+            Console.WriteLine(p.nombre);
+
+            //el tipo de dato que se envia es por referencia
+            Annonymus.cambia(p.nombre);
+            Annonymus.anonimiza(ref p);
+            Console.WriteLine(p.nombre);
+
+            
+        }
+    }
+}
