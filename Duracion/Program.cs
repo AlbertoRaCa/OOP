71a00abfc4caf26b22d50d91188c17881dd8ae44
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-10-09 19:58:41 -0700

    Duracion
    Programa con atributos horas minutos segundos que los imprime

---
+++ Duracion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Duracion
+{
+    class Duracion
+    {
+        private int Horas;
+        private int Minutos;
+        private int Segundos;
+
+        public void SetHoras(int Horas)
+        {
+            this.Horas = Horas;
+        } 
+        public int GetHoras()
+        {
+            return this.Horas;
+        }
+
+        public Duracion(int Horas, int Minutos, int Segundos)
+    {
+        this.Horas = Horas;
+        this.Minutos = Minutos;
+        this.Segundos = Segundos;
+    }
+    
+    public void imprime()
+    {
+        Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}\n", this.Horas, this.Minutos, this.Segundos);
+    }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duracion Pelicula = new Duracion(2,30,0);
+            D.imprime();
+        }
+    }
+}
