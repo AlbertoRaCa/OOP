651ddb388771173ea43255fc6c71dc4122a8ad1a
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-10-10 10:50:20 -0700

    Duracion.1
    Programa que imprime los atributos horas, minutos, segundos de los objetos pelicula, cancion, partido en segundos

--- Duracion/Program.cs
+++ Duracion/Program.cs
@@ -8,33 +8,42 @@ namespace Duracion
         private int Minutos;
         private int Segundos;
 
-        public void SetHoras(int Horas)
-        {
-            this.Horas = Horas;
-        } 
-        public int GetHoras()
-        {
-            return this.Horas;
-        }
-
         public Duracion(int Horas, int Minutos, int Segundos)
     {
         this.Horas = Horas;
         this.Minutos = Minutos;
         this.Segundos = Segundos;
     }
-    
     public void imprime()
     {
-        Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}\n", this.Horas, this.Minutos, this.Segundos);
+        int H;
+        int M;
+        int S;
+        H = (Horas*3600);
+        M = (Minutos*60);
+        S = Segundos;
+        Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}", H, M, S);
+        //Console.WriteLine("Horas en segundos: {0}\n", H);
     }
+    
+    /*public void imprime()
+    {
+        Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}", this.Horas, this.Minutos, this.Segundos);
+    }*/
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Duracion Pelicula = new Duracion(2,30,0);
-            D.imprime();
+            Duracion Pelicula = new Duracion(2,15,12);
+            Console.WriteLine("Pelicula");
+            Pelicula.imprime();
+            Duracion Cancion = new Duracion(0,2,15);
+            Console.WriteLine("\nCancion");
+            Cancion.imprime();
+            Duracion Partido = new Duracion(2,2,15);
+            Console.WriteLine("\nPartido");
+            Partido.imprime();
         }
     }
 }
