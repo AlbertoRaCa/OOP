ef8e6f8e710086f44f09b474299c306e19bf3d06
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-10-10 19:55:30 -0700

    Duracion
    Segunda parte

--- Duracion/Program.cs
+++ Duracion/Program.cs
@@ -14,36 +14,38 @@ namespace Duracion
         this.Minutos = Minutos;
         this.Segundos = Segundos;
     }
-    public void imprime()
+    public void imprimetotal()
     {
-        int H;
         int M;
         int S;
-        H = (Horas*3600);
-        M = (Minutos*60);
-        S = Segundos;
-        Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}", H, M, S);
+        M = (Horas*60)+(Minutos*60);
+        S = (Horas*60*60)+(Minutos*60)+(Segundos);
+        Console.WriteLine("\nTotal en minutos: {0}\nTotal en segundos: {1}", M, S);
         //Console.WriteLine("Horas en segundos: {0}\n", H);
     }
     
-    /*public void imprime()
+    public void imprime()
     {
         Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}", this.Horas, this.Minutos, this.Segundos);
-    }*/
+    }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Duracion Pelicula = new Duracion(2,15,12);
-            Console.WriteLine("Pelicula");
+            Console.WriteLine("Pelicula\n");
             Pelicula.imprime();
-            Duracion Cancion = new Duracion(0,2,15);
+            Pelicula.imprimetotal();
+            
+
+
+            /*Duracion Cancion = new Duracion(0,2,15);
             Console.WriteLine("\nCancion");
             Cancion.imprime();
             Duracion Partido = new Duracion(2,2,15);
             Console.WriteLine("\nPartido");
-            Partido.imprime();
+            Partido.imprime();*/
         }
     }
 }
