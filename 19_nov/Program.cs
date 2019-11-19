e5de2a16f5f929836edcd4b8e95377efb4c88990
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-11-19 12:57:08 -0800

    Class19 nov
    se crea y eliminan archivos en el directorio pór medio de la consola

---
+++ 19_nov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _19_nov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ creadir/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace creadir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Captura el folder a crear");
+            //ruta
+            string directorio = Console.WriteLine();
+            //condicion
+            if(Directory.Exist(directorio))
+            {
+                Console.WriteLine("Ya existe");   
+            }
+            else
+                {
+                    Directory.CreaDiectory(directorio);
+                }
+
+            Directory.CreaDiectory(directorio);
+        }
+    }
+}
